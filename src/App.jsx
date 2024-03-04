2245e390843b80c86fcc7448bb020164e186544f
Megan Grant <granmega@oregonstate.edu>
2024-03-03 23:24:44 -0800

    setting up api using fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
-export default function App() {
-    return (
-        <h1>
-            Penny Candy Store
-        </h1>
-    )
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { fetchProducts } from './Redux/productSlice'
+
+const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/products.json')
+        if (!response.ok) {
+          throw new Error('Could not fetch products')
+        }
+        const data = await response.json()
+        dispatch(fetchProducts(data))
+      } catch (error) {
+        console.error('Error fetching products:', error.message)
+      }
+    }
+
+    fetchData()
+  }, [dispatch])
+
+  return (
+    <div>
+      <h1>Welcome to the Penny Candy Store!</h1>
+    </div>
+  )
 }
+
+export default App;
+
