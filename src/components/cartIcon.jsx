fcd4ee0184e3af37e2999c43579e8e0b30e8e21c
Megan Grant <granmega@oregonstate.edu>
2024-03-07 15:59:02 -0800

    cart is now clickable and stuff is more orderly

--- src/components/cartIcon.jsx
+++ src/components/cartIcon.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const CartIcon = () => {
-  const cartItems = useSelector(state => state.cart);
-  const totalUniqueItems = cartItems.length;
+  const cartItems = useSelector(state => state.cart)
+  const totalUniqueItems = cartItems.length
 
   return (
     <div>
@@ -13,7 +13,7 @@ const CartIcon = () => {
         <span>{totalUniqueItems}</span>
       </Link>
     </div>
-  );
+  )
 }
 
 export default CartIcon;
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './Redux/store';
-import App from './App';
-import Cart from './components/cart';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import React from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './Redux/store'
+import App from './App'
+import Cart from './components/cart'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
 const RoutesComponent = () => {
   return (
